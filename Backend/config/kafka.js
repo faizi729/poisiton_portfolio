442c9e71fca7b20f0ec43f4095594258a6ba2892
faizi729 <f68857440@gmail.com>
2025-11-13 01:01:43 +0530

    helpers added 2

--- Backend/config/kafka.js
+++ Backend/config/kafka.js
@@ -2,14 +2,13 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "trade-service",
-  brokers: [process.env.KAFKA_BROKER], 
+  brokers: ["switchback.proxy.rlwy.net:53752"], 
 });
 
 export const tradeProducer = kafka.producer();
 export const tradeConsumer = kafka.consumer({ groupId: "trade-group" });
 
 export const connectKafka = async () => {
-  console.log("Kafka Broker:", process.env.KAFKA_BROKER);
   try {
     await tradeProducer.connect();
     await tradeConsumer.connect();
