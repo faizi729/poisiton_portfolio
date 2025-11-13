e1443eee3e8c0233e9f3505dfc44e7f2e7528d9d
faizi729 <f68857440@gmail.com>
2025-11-13 17:24:44 +0530

    adding loader for better UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TradeForm from "./pages/trade";
 import Positions from "./pages/position";
 import PnLPage from "./pages/pnl";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
-import { useEffect, useState } from "react";
+
 
 function App() {
-   const [userId, setUserId] = useState(localStorage.getItem("userId"));
+   
 
   // ✅ Listen for login/logout changes in localStorage
-  useEffect(() => {
-    const handleStorageChange = () => {
-      setUserId(localStorage.getItem("userId"));
-    };
-
-    window.addEventListener("storage", handleStorageChange);
-    return () => window.removeEventListener("storage", handleStorageChange);
-  }, []);
-
+ 
   return (
     <BrowserRouter>
       <Routes>
